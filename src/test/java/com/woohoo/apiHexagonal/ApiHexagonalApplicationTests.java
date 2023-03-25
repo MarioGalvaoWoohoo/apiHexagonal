b6077f7b5695f4faadc8b3bca679c96b2cf28490
MarioGalvaoWoohoo <65731257+MarioGalvaoWoohoo@users.noreply.github.com>
2023-03-25 15:51:23 -0300

    Primeiro commit com o projeto spring limpa

---
+++ src/test/java/com/woohoo/apiHexagonal/ApiHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woohoo.apiHexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
